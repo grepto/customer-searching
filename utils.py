ec631192a3492e3d59825cffdd68e290d927ea4b
grepto <grepto@gmail.com>
2019-06-26 11:33:13 +0300

    rename argument list to list_to_filter

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import datetime
 
 
-def get_filtered_list(list, key, timedelta):
+def get_filtered_list(list_to_filter, key, timedelta):
     now = datetime.datetime.now()
     min_date = now - timedelta
-    filtered_list = [item for item in list if item[key] > int(min_date.timestamp())]
+    filtered_list = [item for item in list_to_filter if item[key] > int(min_date.timestamp())]
     return filtered_list
